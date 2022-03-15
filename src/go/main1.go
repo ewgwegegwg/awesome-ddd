1a1c5c2cecbff6c60a48d900608e87b2dca11621
ewgwegegwg <113235102+ewgwegegwg@users.noreply.github.com>
2022-03-15 23:38:35 +0000

    add test case (2022-03-15T23:38:35)

--- src/go/main1.go
+++ src/go/main1.go
@@ -3,3 +3,4 @@ package main\nimport "fmt"\nfunc main() { fmt.Println("update 2022-02-06T11:15:2
 package main\nimport "fmt"\nfunc main() { fmt.Println("update 2022-02-17T03:33:42") }
 package main\nimport "fmt"\nfunc main() { fmt.Println("update 2022-02-17T22:51:08") }
 package main\nimport "fmt"\nfunc main() { fmt.Println("update 2022-03-07T16:46:48") }
+package main\nimport "fmt"\nfunc main() { fmt.Println("update 2022-03-15T23:38:35") }
